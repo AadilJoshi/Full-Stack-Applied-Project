c022ffae86b37441bf3f6e587a54d9fc62358cb7
Aadil Joshi <22161355@student.westernsydney.edu.au>
2026-06-03 23:31:19 +1000

    Still fixing the database

---
+++ app/api/products/route.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+// GET all products
+export async function GET() {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
+}
+
+// CREATE product (admin)
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      description: body.description,
+      price: Number(body.price),
+      category: body.category,
+      image: body.image,
+    },
+  });
+
+  return NextResponse.json(newProduct);
+}
+
+// DELETE product
+export async function DELETE(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const id = Number(searchParams.get("id"));
+
+  await prisma.product.delete({
+    where: { id },
+  });
+
+  return NextResponse.json({ message: "Deleted successfully" });
+}
