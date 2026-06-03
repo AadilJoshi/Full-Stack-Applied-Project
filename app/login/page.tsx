daf3d0486dc8b5d9e726b97616c67fac0a6ce4e0
Aadil Joshi <22161355@student.westernsydney.edu.au>
2026-06-03 20:56:55 +1000

    Making a login page

---
+++ app/login/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    const success = login(username, password);
+
+    if (success) {
+      router.push("/");
+    } else {
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <p>Test accounts:</p>
+      <p>user / user</p>
+      <p>admin / admin</p>
+    </main>
+  );
+}
